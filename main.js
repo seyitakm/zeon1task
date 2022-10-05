fec7efabb18c0e996adabeb51a8ad67c57c532d5
seyitakm <seyit200020@gmail.com>
2022-10-05 14:03:41 +0600

    footer

---
+++ main.js
@@ -0,0 +1,14 @@
+window.onscroll = function () {
+  myFunction();
+};
+
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
