ed6992ecadd191d114ff625d2641e66e7788bcc7
seyitakm <seyit200020@gmail.com>
2022-10-06 18:18:11 +0600

    added newPage

---
+++ aboutUs.js
@@ -0,0 +1,6 @@
+let logo = document.getElementById("logoNav");
+
+console.log(logo);
+logo.addEventListener("click", () => {
+  window.location.href = "./index.html";
+});
--- main.js
+++ main.js
@@ -5,6 +5,11 @@ window.onscroll = function () {
 let navbar = document.getElementById("navbar");
 let sticky = navbar.offsetTop;
 
+let aboutUsPage = document.getElementById("aboutUsNav");
+aboutUsPage.addEventListener("click", () => {
+  window.location.href = "./aboutUs.html";
+});
+
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky");
